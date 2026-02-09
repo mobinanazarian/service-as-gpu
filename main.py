1b5ff7c452879dbf75c4a00b55fb2728f65f28f0
mobinanazarian <mobinanazarian5@gmail.com>
2026-02-09 22:56:42 +0330

    remove main.py

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-class Job:
-    def init(self, name, hours):
-        self.name = name
-        self.hours = hours
-        self.status = "PENDING"
 
-    def run(self):
-        self.status = "COMPLETED"
-
-
-job1 = Job("test-job", 2)
-print(job1.name)
-print(job1.status)
-
-job1.run()
-print(job1.status)
